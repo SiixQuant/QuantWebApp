b1e8504b02094e35b1a5e955f594a64e5712a66a
Siix <Gianni_gonzalez@yahoo.com>
2024-11-11 09:53:56 -0800

    Delete Quant Web App directory

--- Quant Web App/run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-tasklist /FI "IMAGENAME eq streamlit.exe" 2>NUL | find /I /N "streamlit.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-    echo Streamlit is already running
-    exit
-) else (
-    start streamlit run "C:\Users\Giann\OneDrive\Desktop\Quant Web App\app.py"
-)
-pause 
